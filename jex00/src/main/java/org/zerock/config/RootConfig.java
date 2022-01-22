8a8f5d57a4641ba126de400477c989cf6c1bba46
yurina <yurina@yurina-PC>
2022-01-22 21:18:43 +0900

    mariadb connection

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -1,10 +1,29 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.zerock.sample"})
 public class RootConfig {
+	
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("org.mariadb.jdbc.Driver");
+		hikariConfig.setJdbcUrl("jdbc:mariadb://127.0.0.1:3306/yurina");
+		hikariConfig.setUsername("yurina");
+		hikariConfig.setPassword("yurina");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
 
 }
